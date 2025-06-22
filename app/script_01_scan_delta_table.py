c6fd174ac100958211151c7e71688912b693e929
monkinng9 <T_chayut@kkumail.com>
2025-06-22 14:51:34 +0700

    feat: add script to scan and export delta table to CSV using Polars

---
+++ app/script_01_scan_delta_table.py
@@ -0,0 +1,12 @@
+import polars as pl
+import pathlib
+
+table_path = "data/demo_data/processed/product_delta"
+
+delta_table = pl.scan_delta(table_path)
+
+dirpath = "data/demo_data/processed/delta_to_csv"
+
+path = "data/demo_data/processed/product_delta.csv"
+
+delta_table.collect().write_csv(path, separator=",")
